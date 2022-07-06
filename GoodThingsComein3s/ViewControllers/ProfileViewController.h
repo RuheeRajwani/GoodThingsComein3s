6be733dfd179d63b1a74ed08084dc0324a5af709
Ruhee Rajwani <ruheer@fb.com>
2022-07-06 14:40:05 -0700

    Completed App Skeleton

---
+++ GoodThingsComein3s/ViewControllers/ProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.h
+//  GoodThingsComein3s
+//
+//  Created by Ruhee Rajwani on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GoodThingsComein3s/ViewControllers/SearchViewController.h
@@ -0,0 +1,16 @@
+//
+//  SearchViewController.h
+//  GoodThingsComein3s
+//
+//  Created by Ruhee Rajwani on 7/6/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SearchViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
