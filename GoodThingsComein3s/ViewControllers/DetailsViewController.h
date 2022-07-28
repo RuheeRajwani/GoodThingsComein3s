4ebc54680e98631755597d35decfc73c668611ab
RuheeRajwani <ruhee.rajwani@gmail.com>
2022-07-28 13:27:21 -0700

    Added details page UI and outlets

---
+++ GoodThingsComein3s/ViewControllers/DetailsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailsViewController.h
+//  GoodThingsComein3s
+//
+//  Created by Ruhee Rajwani on 7/28/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GoodThingsComein3s/ViewControllers/DetailsViewController.m
@@ -0,0 +1,27 @@
+//
+//  DetailsViewController.m
+//  GoodThingsComein3s
+//
+//  Created by Ruhee Rajwani on 7/28/22.
+//
+
+#import "DetailsViewController.h"
+
+@interface DetailsViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *restaurantNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *restaurantPriceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *restaurantCategoriesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *restaurantHoursLabel;
+@property (weak, nonatomic) IBOutlet UILabel *restaurantAddressLabel;
+@property (weak, nonatomic) IBOutlet UICollectionView *restaurantPhotosCollectionVIew;
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
